6dabbff765d708a735ff80bf859e6e837971e631
Mattias Giese <giese@b1-systems.de>
2015-03-12 17:17:21 +0100

    some more stuff

--- foreman-ubuntu/test-deploy.sh
+++ foreman-ubuntu/test-deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -x 
-echo Hello World
 echo "deb http://deb.theforeman.org/ trusty 1.8" > /etc/apt/sources.list.d/foreman.list
 echo "deb http://deb.theforeman.org/ plugins 1.8" >> /etc/apt/sources.list.d/foreman.list
 wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb -O /tmp/puppetlabs-release-trusty.deb
-dpkg -i /tmp/puppetlabs-release-trusty.deb
+dpkg -i /tmp/puppetlabs-release-trusty.deb && rm /tmp/puppetlabs-release-trusty.deb
 
 apt-get update
 ##apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'  -f -q -y install foreman-installer
@@ -14,6 +12,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install \
 	-o Dpkg::Options::='--force-confold'  -y -f -q --force-yes \
 	foreman-installer puppet
 
+cp /vagrant/foreman-answers-demo.yaml /etc/foreman/foreman-installer-answers.yaml
 foreman-installer
 service puppet restart
 
